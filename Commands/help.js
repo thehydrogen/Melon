e091b78b3590061a14c276fa837a70f519e3aca7
BriciWorld <30957824+BriciWorldyt@users.noreply.github.com>
2018-04-27 11:08:41 +0300

    Add files via upload

---
+++ Commands/help.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+exports.run = (client, msg, args, config) => {
+    
+
+let embed = new Discord.RichEmbed()
+       
+.setAuthor("Melon", client.user.avatarURL)
+.setColor("#d45555")
+.setDescription("Here you can see the commands for Melon!")
+.addField(`+help`, "Shows this list")
+.addField(`+rps`, "Play rock, paper, scissors!")
+.addField(`+8ball`, "Ask something to the magic 8ball. Usage: `+8ball <question>`")
+.addField(`+ping`, "Shows the latency ping")
+.addField(`+serverinfo`, "Displays information of the current server")
+.addField(`+avatar`, "Shows your avatar")
+.addField(`+info`, "Displays information of this bot")
+.addField(`+cat`, "Shows a random cat (API broken)")
+.addField(`+dog`, "Shows a random dog")
+.addField(`+say`, "Says something to the chat! Usage: `+say <text>`")
+.addField(`+credits`, "Shows all Melon developers and APIs the bot is using")
+.addField(`+uptime`, "Displays bot's uptime")
+.addField(`+slots`, "Play the slots!")
+.addField(`+embed`, "Says something to the chat, but this time with embed! Usage: `+embed <text>`")
+.addField(`+profile`, "Displays information of your account")
+
+msg.author.send(embed);
+msg.channel.send({embed: {
+        color: 0xd45555,
+        title: `Check DMs :mailbox:`,
+        }
+    });
+}
+config: {}
---
+++ Commands/info.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+
+exports.run = (client, msg, args) => {
+    function format(seconds){
+        function pad(s){
+          return (s < 10 ? '0' : '') + s;
+        }
+        var hours = Math.floor(seconds / (60*60));
+        var minutes = Math.floor(seconds % (60*60) / 60);
+        var seconds = Math.floor(seconds % 60);
+      
+        return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+      }
+      
+      var uptime = process.uptime();
+
+    msg.channel.send({embed: {
+        color: 0x00ff00,
+        author: {
+          name: "Melon",
+          icon_url: client.user.avatarURL
+        },
+        description: `${client.user.username} - A discord bot that does everything!`,
+        fields: [{
+            name: "Author",
+            value: `BriciWorld#1434`,
+           inline: true
+          },
+          {
+            name: "Servers",
+            value: `${client.guilds.size}`,
+            inline: true
+          },
+          {
+            name: "Users",
+            value: `${client.users.size}`,
+            inline: true
+          },
+          {
+            name: "Channels",
+            value: `${client.channels.size}`,
+            inline: true
+          },
+          {
+            name: "Bot version",
+            value: `v0.6b`,
+            inline: true
+          },
+          {
+            name: "Discord.js version",
+            value: `v${Discord.version}`,
+            inline: true
+          },
+          {
+            name: "Node.js version",
+            value: `${process.version}`,
+            inline: true
+          },
+          {
+            name: "Uptime",
+            value: `${format(uptime)}`,
+            inline: true
+          },
+          {
+            name: "Memory Usage",
+            value: (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + `MB`,
+            inline: true
+          },
+          {
+            name: "Support Server",
+            value: `[Join](https://discord.gg/H5YDACD)`,
+            inline: true
+          },
+        ],
+        footer: {
+          text: `All systems operational`
+            }
+        }
+    });
+    console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
+    };
+  config: {}
