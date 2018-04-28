ecf5088439e51814c31bb2cfa6d2512c2bbf538d
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-04-28 14:26:57 +0200

    🖼 Avatar Command Update

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -1,13 +1,27 @@
 exports.run = (client, msg, args) => {
+    const user = msg.mentions.users.first()
+    
+    if(user) {
+        msg.channel.send({embed: {
+        color: 0x00ff00,
+            title: `:frame_photo: ${user.username}'s Avatar`,
+            description: `Image isn't loading? [Click here](${user.avatarURL})`,
+            image: {
+                url: user.avatarURL
+            },
+            }
+        });
+    } else {
     msg.channel.send({embed: {
         color: 0x00ff00,
             title: `:frame_photo: Your Avatar`,
-            description: `Image not loaded? [Click here](${msg.author.avatarURL})`,
+            description: `Image isn't loading? [Click here](${msg.author.avatarURL})`,
             image: {
                 url: msg.author.avatarURL
             },
             }
         });
+    }
     console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
     };
   config: {}
