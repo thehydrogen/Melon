dde5a253013bed8a621294180deb074d77fbaeef
BriciWorld <30957824+BriciWorldyt@users.noreply.github.com>
2018-05-03 20:54:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`Serving in ${client.guilds.size} servers, with ${client.users.size} users on ${client.channels.size} channels`)
     client.user.setStatus(`online`)
-    client.user.setActivity(`Type +help | v1.0`,  { type: 'STREAMING', url: `https://twitch.tv/briciworld` });
+    client.user.setActivity(`Type +help | v1.1`,  { type: 'STREAMING', url: `https://twitch.tv/briciworld` });
 });
 
 fs.writeFile('./userData.json', JSON.stringify(userData), (err) => {
