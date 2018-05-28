b99a6f0f8e01d2cc42801ab1a98a99ff7e7af143
BW Tech <30957824+bwtechyt@users.noreply.github.com>
2018-05-28 11:36:51 +0300

    Update shutdown.js

--- Commands/shutdown.js
+++ Commands/shutdown.js
@@ -1,6 +1,7 @@
 exports.run = (client, msg, args) => {
 
-    if(msg.author.id !== "") return msg.channel.send({embed: {
+    let maintainers = ["", ""]; 
+    if(!maintainers.includes(msg.author.id)) return msg.channel.send({embed: {
         color: 0xff0000,
          title: `:no_entry: Oops!`,
             description: `You don't have permission to use this command. You need to be **Bot Host** in order to use this command!`,
