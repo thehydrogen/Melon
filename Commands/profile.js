51dcb2eb31397d458c878b032fb5d0662dea60f9
BW Tech <30957824+bwtechyt@users.noreply.github.com>
2018-05-28 11:34:32 +0300

    You can now see someone else's profile (same thing as the avatar command)

--- Commands/profile.js
+++ Commands/profile.js
@@ -1,6 +1,37 @@
 const Discord = require("discord.js");
 
 exports.run = (client, msg, args) => {
+  const user = msg.mentions.users.first()
+    
+    if(user) {
+      msg.channel.send({embed: {
+        color: 0xd45555,
+        title: `:bust_in_silhouette: User information:`,
+        thumbnail: {
+          url: user.avatarURL
+           },
+          fields: [{
+            name: "Username & Discriminator",
+            value: `@**${user.tag}**`
+          },
+          {
+            name: "Created",
+            value: `${user.createdAt}`
+          },
+          {
+            name: "Bot?",
+            value: `${user.bot}`,
+            inline: true
+          },
+          {
+            name: ":id:",
+            value: `${user.id}`,
+            inline: true
+          },
+        ],
+        }
+    });
+    } else {
 msg.channel.send({embed: {
     color: 0xd45555,
     title: `:bust_in_silhouette: User information:`,
@@ -28,6 +59,7 @@ msg.channel.send({embed: {
     ],
     }
 });
+    }
 console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
 };
 config: {}
