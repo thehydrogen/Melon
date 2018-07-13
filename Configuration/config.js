5c61d6a6e06a315c4e625cbd10e3387b6bdbe963
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-07-13 14:37:57 +0200

    ⚙️ - Configuration

---
+++ Configuration/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    bot: {
+        user: {
+            status: 1, // 1 = Online | 2 = Away | 3 = Do Not Disturb
+            game: "{bot_name} - ${guilds_size} servers" // {bot_name} = The bot's name | {guilds_size} = How many servers the bot is in.
+        },
+        
+        // --------
+        
+        core: { // DO NOT CHANGE ANYTHING IN HERE
+            version: "2.3.4",
+            build: "12b"
+        },
+        
+        // --------
+        
+        branch: "2",
+        
+        // --------
+        
+        version: "2.0",
+        
+        // --------
+        
+        maintainers: ["Insert", "maintainers", "here"],
+        
+        // --------
+        
+        defaultDBConfig: {
+            prefix: "+",
+            // --------
+            strikes: {},
+            // --------
+            welcomeMessages: false,
+            welcomeChannel: "general",
+            welcomeMessage: "Welcome, {user}!",
+            // --------
+            leaveMessages: false,
+            leaveChannel: "general",
+            leaveMessage: "Farewell, {user}."
+            // --------
+            eventLogger: true,
+            eventLoggerChannel: "logs",
+            // --------
+            messageLogger: false,
+            messageLoggerChannel: "logs"
+        }
+    }
+};
