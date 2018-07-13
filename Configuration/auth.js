0b855f21bdb27eae446f1ac49bb194bd92e7e7e4
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-07-13 14:27:37 +0200

    ⚙️ - Redisigned JS-based authentication config

---
+++ Configuration/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+    discord: {
+        token: "", // Discord API Token ~ Used to connect to the Discord API. KEEP SECRET!
+        secret: "" // Discord API Secret ~ Not exactly sure what this does. KEEP SECRET!
+    },
+    
+    apiTokens: {} // To be done™
+};
