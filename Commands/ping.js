1ada9b02124904dea9224282ce5356029bb2040f
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-07-21 22:35:12 +0200

    🏓 - Ping Pong!

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,31 +1,5 @@
-exports.run = (client, msg, args) => {
-    function format(seconds){
-        function pad(s){
-          return (s < 10 ? '0' : '') + s;
-        }
-        var hours = Math.floor(seconds / (60*60));
-        var minutes = Math.floor(seconds % (60*60) / 60);
-        var seconds = Math.floor(seconds % 60);
-      
-        return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
-      }
-      
-      var uptime = process.uptime();
-
-    msg.channel.send(`Please wait until we check the bot's latency...`).then(function(m) {
-        m.edit({embed: {
-            color: 0xd45555,
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL
-            },
-            description: `:ping_pong: **Melon** by BriciWorld, Casbas 234, and Hydrogen, is running for ${format(uptime)}. Serving ${client.users.size} users in ${client.guilds.size} servers. :dizzy:`,
-            footer: {
-              text: `This took me ${m.createdTimestamp - msg.createdTimestamp}ms`
-                }
-            }
-        });
-    console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
+module.exports = (msg, client) => {
+    msg.chnnnel.send(":ping_pong: Pong?").then(ctx => {
+        ctx.edit(`:ping_pong: Pong!\n:heartbeat: **Heartbeat:** \`${client.ping}ms\`\n:notepad_spiral: **Message Edit:** \`${ctx.createdTimestamp - msg.createdTimestamp}\``)
     })
 };
-  config: {}
