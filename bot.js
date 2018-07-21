f730bf76536440eed9afa87e248f9bf9fc49b4ad
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-07-21 22:41:52 +0200

    📝 - DB

--- bot.js
+++ bot.js
@@ -13,41 +13,47 @@ const config = require("./Configuration/config.js");
 const auth = require("./Configuration/auth.js");
 const embed = new Discord.RichEmbed();
 
-client.on('ready', () => { 	
-    console.log(`[boot] Weehoo! I've connected to Discord as ${client.user.usernmae} (${client.user.id}).`);
-    
-    game = config.bot.user.game.replace("{bot_name}", client.user.username).replace("{guilds_size}", client.guilds.size)
-    client.user.setActivity(config.bot.user.game);
-    
-    let status = config.bot.user.status;
-    switch (status) {
-        case 1:
-            status = "ONLINE";
-            break;
-        case 2:
-            status = "AWAY";
-            break;
-        case 3:
-            status = "DND";
-            break;
-        default:
-            status = "ONLINE";
-            console.log("[status_err] The status you've chosen in your configuration file could not be recognazied. I have defaulted to ONLINE.");
-            break;
-    }
-    
-    client.user.setStatus(status);
-})
+const Enmap = require("enmap");
+const level = require("enmap-level");
+const guilds = new Enmap({ provider: new level({ name: "guilds", dbName: client.user.username, dataDir: "./data/" }) })
 
-client.on("guildDelete", guild => {
-    if (disabledEvents.includes("guildDelete")) return;
-    if(!guild.available) return;
-    console.log("I've left a guild.");
+guilds.defer.then(() => {
+    client.on('ready', () => { 	
+        console.log(`[boot] Weehoo! I've connected to Discord as ${client.user.usernmae} (${client.user.id}).`);
+
+        game = config.bot.user.game.replace("{bot_name}", client.user.username).replace("{guilds_size}", client.guilds.size)
+        client.user.setActivity(config.bot.user.game);
+
+        let status = config.bot.user.status;
+        switch (status) {
+            case 1:
+                status = "ONLINE";
+                break;
+            case 2:
+                status = "AWAY";
+                break;
+            case 3:
+                status = "DND";
+                break;
+            default:
+                status = "ONLINE";
+                console.log("[status_err] The status you've chosen in your configuration file could not be recognazied. I have defaulted to ONLINE.");
+                break;
+        }
+
+        client.user.setStatus(status);
+    })
+
+    client.on("guildDelete", guild => {
+        if (disabledEvents.includes("guildDelete")) return;
+        if(!guild.available) return;
+        console.log("I've left a guild.");
+    });
+
+    client.on("message", msg => {
+        if (disabledEvents.includes("message")) return;
+        require("./CommandHandler.js")(msg, client);
+    });
+
+    client.login(auth.discord.token);
 });
-
-client.on("message", msg => {
-    if (disabledEvents.includes("message")) return;
-    require("./CommandHandler.js")(msg, client);
-});
-
-client.login(auth.discord.token);
