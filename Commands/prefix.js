fb81c8c40f1f4338ba94fc855385a8e8528afb8d
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-07-21 23:27:37 +0200

    🔧 - Prefix Command

---
+++ Commands/prefix.js
@@ -0,0 +1,32 @@
+module.exports = (msg, client) => {
+    if (msg.member.hasPermission("MANAGE_SERVER")) {
+        if (msg.suffix) {
+            let guild = client.DB.get(msg.guild.id)
+            guild.prefix = msg.suffix;
+            client.DB.set(msg.guild.id, guild);
+            msg.channel.send({
+                embed: {
+                    title: `Success!`,
+                    description: `Your prefix has been changed to \`${msg.suffix}\``
+                    color: 0xff0000
+                }
+            })
+        } else {
+            msg.channel.send({
+                embed: {
+                    title: `Error!`,
+                    description: `\`prefix\` is a required argument that is missing.`,
+                    color: 0xff0000
+                }
+            })
+        }
+    } else {
+        msg.channel.send({
+            embed: {
+                title: `Error!`,
+                description: `This command is a higher rank than you.`,
+                color: 0xff0000
+            }
+        })
+    }
+};
