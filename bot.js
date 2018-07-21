66c92fff4d2eac450fd541ad7922d0ce9ad313d2
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-07-21 21:22:00 +0200

    🔧 - Main file code changes

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 
-console.log("[boot] Booting up... If you're stuck at this screen, something has gone wrong.")
+console.log("[boot] Booting up... If you're stuck at this screen, something has gone wrong.");
+
+disabledEvents = ["message"];
 
 // Imports
 const fs = require("fs");
@@ -7,36 +9,44 @@ const Discord = require("discord.js");
 const random = require("random-animal");
 const weather = require("weather-js");
 const client = new Discord.Client();
-const config = require("./Configuration/auth.json");
+const config = require("./Configuration/config.js");
+const auth = require("./Configuration/auth.js");
 const embed = new Discord.RichEmbed();
 
 client.on('ready', () => { 	
     console.log(`[boot] Weehoo! I've connected to Discord as ${client.user.usernmae} (${client.user.id}).`);
+    
+    game = config.bot.user.game.replace("{bot_name}", client.user.username).replace("{guilds_size}", client.guilds.size)
     client.user.setActivity(config.bot.user.game);
-});
-
-client.on("guildCreate", guild => {
-  console.log(`[guildCreate] I've just been added to a new guild! Here's all the info about it:\n           Owner: ${guild.owner.user.tag} | Members: ${guild.memberCount} | Name: ${guild.name}`);
-});
+    
+    let status = config.bot.user.status;
+    switch (status) {
+        case 1:
+            status = "ONLINE";
+            break;
+        case 2:
+            status = "AWAY";
+            break;
+        case 3:
+            status = "DND";
+            break;
+        default:
+            status = "ONLINE";
+            console.log("[status_err] The status you've chosen in your configuration file could not be recognazied. I have defaulted to ONLINE.");
+            break;
+    }
+    
+    client.user.setStatus(status);
+})
 
 client.on("guildDelete", guild => {
-  if(!guild.available) return;
-  console.log(`I've left: ${guild.name}. This could've been because I was kicked, banned, or the server was deleted.`);
+    if (disabledEvents.includes("guildDelete")) return;
+    if(!guild.available) return;
+    console.log("I've left a guild.");
 });
 
 client.on("message", msg => {
-    if (msg.author.bot) return;
-    if(msg.content.indexOf(config.prefix) !== 0) return;
-  
-    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-  
-    try {
-      let commandFile = require(`./Commands/${command}.js`);
-      commandFile.run(client, msg, args);
-    } catch (err) {
-      console.error(err);
-    }
-  });
+    if (disabledEvents.includes("message")) return;
+});
 
-client.login(config.token);
+client.login(auth.discord.token);
