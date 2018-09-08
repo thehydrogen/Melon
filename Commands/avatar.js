9e2bf3e03aeabdceb220c3119d5ff001cfe55e50
Ken <34059898+thehydrogen@users.noreply.github.com>
2018-09-08 17:43:16 +0200

    🔧 - Compatibility update for avatar.js

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -1,9 +1,9 @@
-exports.run = (client, msg, args) => {
+module.exports = (client, msg) => {
     const user = msg.mentions.users.first()
     
     if(user) {
         msg.channel.send({embed: {
-        color: 0x00ff00,
+            color: 0x00ff00,
             title: `:frame_photo: ${user.username}'s Avatar`,
             description: `Image isn't loading? [Click here](${user.avatarURL})`,
             image: {
@@ -12,16 +12,14 @@ exports.run = (client, msg, args) => {
             }
         });
     } else {
-    msg.channel.send({embed: {
-        color: 0x00ff00,
-            title: `:frame_photo: Your Avatar`,
-            description: `Image isn't loading? [Click here](${msg.author.avatarURL})`,
-            image: {
-                url: msg.author.avatarURL
-            },
+        msg.channel.send({embed: {
+                color: 0x00ff00,
+                title: `:frame_photo: Your Avatar`,
+                description: `Image isn't loading? [Click here](${msg.author.avatarURL})`,
+                image: {
+                    url: msg.author.avatarURL
+                },
             }
         });
     }
-    console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
-    };
-  config: {}
+};
