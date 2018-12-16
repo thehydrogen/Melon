7eedce6f9126d22ac59547c27a4569486b6b004d
Hydrogen <yt.thetechguy@gmail.com>
2018-12-16 13:37:21 +0100

    ✨ Embed compatability complete

--- Commands/embed.js
+++ Commands/embed.js
@@ -1,18 +1,13 @@
-const Discord = require("discord.js");
-
-exports.run = (client, msg, args) => {
-
-    let text = args.join(" ");
-    msg.delete(),
-msg.channel.send({embed: {
-    color: 0xd45555,
-    author: {
-        name: msg.author.username + " says...",
-        icon_url: msg.author.avatarURL
-      },
-description: text,
-    }
-});
-console.log(`Treating ${msg.content} by ${msg.author.tag} from ${msg.guild} as a command.`);
+module.exports = (msg, client) => {
+    let suffix = msg.suffix;
+    msg.delete();
+    msg.channel.send({embed: {
+        color: 0xd45555,
+        author: {
+            name: msg.author.username + 'says',
+            icon_url: msg.author.avatarURL
+        },
+        description: text,
+        }
+    });
 };
-config: {}
